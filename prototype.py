88215dc6cefe3c2f7f4b11bbe58922fac387ec92
KaranAgrawal1509 <90572559+KaranAgrawal1509@users.noreply.github.com>
2023-03-24 20:18:53 -0400

    3.5 in Use

--- prototype.py
+++ prototype.py
@@ -1,61 +1,40 @@
 import streamlit as st
 import openai
-import os
 
-openai.api_key =  st.secrets["Open_AI_key"]
+openai.api_key = st.secrets["Open_AI_key"]
 
-def translate_text(text, target_language):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Translate this text into {target_language}: {text}",
-        temperature=0.5,
+def process_text(text, prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": text}
+        ],
         max_tokens=1024,
-        n = 1,
+        n=1,
         stop=None
     )
-    return response.choices[0].text.strip()
-
-def analyze_sentiment(text):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Analyze the sentiment of this text: {text}",
-        temperature=0.5,
-        max_tokens=1024,
-        n = 1,
-        stop=None
-    )
-    return response.choices[0].text.strip()
-
-def generate_text(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=1024,
-        n = 1,
-        stop=None
-    )
-    return response.choices[0].text.strip()
+    return response.choices[0].message['content'].strip()
 
 st.title("Welcome to the Multi-Purpose Chatbot!")
 
-text = st.text_input("Enter your text message:")
+text = st.text_input("Enter your text message (max 50 characters):", max_chars=50)
 
 action = st.selectbox("What do you want to do with your text?", ("", "Translate", "Analyze Sentiment", "Generate Text"))
 
 if action == "Translate":
     target_language = st.text_input("Enter target language:")
     if st.button("Translate Text"):
-        translated_text = translate_text(text, target_language)
+        translated_text = process_text(text, f"Translate this text into {target_language}:")
         st.write("Translated Text:", translated_text)
 
 elif action == "Analyze Sentiment":
     if st.button("Analyze Sentiment"):
-        sentiment = analyze_sentiment(text)
+        sentiment = process_text(text, "Analyze the sentiment of this text:")
         st.write("Sentiment Analysis:", sentiment)
 
 elif action == "Generate Text":
-    prompt = st.text_input("Enter prompt:")
+    prompt = st.text_input("Enter prompt (max 50 characters):", max_chars=50)
     if st.button("Generate Text"):
-        generated_text = generate_text(prompt)
+        generated_text = process_text(prompt, "")
         st.write("Generated Text:", generated_text)
